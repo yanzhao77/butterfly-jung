4e12d4a1413522b6120c0d33d786c71c76611df0
Azir <794070011@qq.com>
2024-10-25 10:49:19 +0800

    添加入口类

--- src/test/java/com/softroad/butterflyjung/ButterflyJungApplicationTests.java
+++ src/test/java/com/softroad/butterflyjung/ButterflyJungApplicationTests.java
@@ -1,9 +1,6 @@
 package com.softroad.butterflyjung;
 
-import com.softroad.butterflyjung.layouts.ForceDirected;
 import org.junit.jupiter.api.Test;
-
-import static com.softroad.butterflyjung.layouts.ForceDirected.testGraph;
 
 
 
@@ -11,7 +8,7 @@ class ButterflyJungApplicationTests {
 
     @Test
     void contextLoads() {
-        new ForceDirected(testGraph(), 1536, 1024).show();
+//        new ForceDirected(testGraph(), 1536, 1024).show();
     }
 
 }
